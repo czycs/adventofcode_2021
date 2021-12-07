74562277883dc0554b5860f68fffc0f760ee91e7
czycs <95682736+czycs@users.noreply.github.com>
2021-12-07 10:53:58 +0100

    day_7

---
+++ day_7.R
@@ -0,0 +1,45 @@
+#Adventofcode Day 7
+
+crab_subs <-
+  read.table(
+    "C:/Users/lagij/Desktop/adventofcode/input_7.txt",
+    sep = ",",
+    quote = "\"",
+    comment.char = ""
+  )
+crab_subs_vec <- as.numeric(crab_subs[1, ])
+#plot(density(crab_subs_vec))
+
+testinput <- c(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
+#Part1
+median(testinput)
+median(crab_subs_vec)
+
+
+#Part2
+fuel_usage <- function(vec) {
+  least_fuel = 1000000000
+  for (z in min(crab_subs_vec):max(crab_subs_vec)) {
+    fuel = 0
+    
+    for (i in vec) {
+      #Part1:
+      #fuel <- fuel + abs(z - i)
+      #Part2:
+      fuel <- fuel + sum(1:abs(z - i))
+      if (fuel > least_fuel) {
+        break
+      }
+    }
+    if (fuel < least_fuel) {
+      least_fuel <- fuel
+      #print(z)
+    }
+  }
+  return(least_fuel)
+  
+}
+
+
+fuel_usage(testinput)
+fuel_usage(crab_subs_vec)
