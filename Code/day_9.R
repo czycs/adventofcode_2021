e16ec7f51e493b14515cb2f56cf18bdf5d244da3
czycs <95682736+czycs@users.noreply.github.com>
2021-12-09 11:35:49 +0100

    Part 2

--- Code/day_9.R
+++ Code/day_9.R
@@ -104,9 +104,62 @@ find_low_points <- function(df) {
   }
   return(output_vec)
 }
-find_low_points(testmatrix)
+
 
 result1<-sum(find_low_points(mat_num)+1)
 
 #Part2
+
+
+nine_mat<-matrix(rep(9,102*102),ncol = 102,nrow = 102)
+
+
+nine_mat[2:101,2:101]<-mat_num
+
+seen_mat<-`dim<-`(nine_mat %in% c(9), dim(nine_mat))
+
+basin_mat<-nine_mat
+basin_mat[nine_mat==9]<-0
+  
+basin_search<-function(df,row_ind,col_ind,basin_id){
+  seen_mat[row_ind,col_ind]<<-TRUE
+  basin_mat[row_ind,col_ind]<<-basin_id
+  if(!seen_mat[row_ind-1,col_ind]){
+    basin_mat<-basin_search(df,row_ind-1,col_ind,basin_id)
+  }
+  if(!seen_mat[row_ind+1,col_ind]){
+    basin_mat<-basin_search(df,row_ind+1,col_ind,basin_id)
+  }
+  if(!seen_mat[row_ind,col_ind-1]){
+    basin_mat<-basin_search(df,row_ind,col_ind-1,basin_id)
+  }
+  if(!seen_mat[row_ind,col_ind+1]){
+    basin_mat<-basin_search(df,row_ind,col_ind+1,basin_id)
+  }
+  basin_mat
+}
+
+basin_lowpoint <- function(df) {
+  #Find low point
+  basin_id = 0
+  for (row_ind in 2:101) {
+    for (col_ind in 2:101) {
+      if (df[row_ind, col_ind] < df[row_ind - 1, col_ind]) {
+        if (df[row_ind, col_ind] < df[row_ind + 1, col_ind]) {
+          if (df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            if (df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
+              #df[row_ind,col_ind] is low point
+              basin_id = basin_id + 1
+              basin_search(df, row_ind, col_ind, basin_id)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+basin_lowpoint(nine_mat)
+
+
+
 
