38a176945a1ebcb98ff494311227de747cdd3b39
czycs <95682736+czycs@users.noreply.github.com>
2021-12-10 08:00:47 +0100

    Fixed spelling

--- Code/day_9.R
+++ Code/day_9.R
@@ -15,90 +15,82 @@ cave_table <-
 split_list <- strsplit(cave_table[, 1], "")
 split_df <- do.call(rbind.data.frame, split_list)
 colnames(split_df) <- c(1:100)
-mat_num<-matrix(as.numeric(unlist(split_df)),ncol = ncol(split_df))
-
-testmatrix<-matrix(c(2,1,9,9,9,4,3,2,1,0,
-                     3,9,8,7,8,9,4,9,2,1,
-                     9,8,5,6,7,8,9,8,9,2,
-                     8,7,6,7,8,9,6,7,8,9,
-                     9,8,9,9,9,6,5,6,7,8),ncol = 10,byrow = TRUE)
+mat_num <- matrix(as.numeric(unlist(split_df)), ncol = ncol(split_df))
 
 find_low_points <- function(df) {
   output_vec <- c()
-
+  
   for (row_ind  in c(1:100)) {
-
-    for(col_ind in c(1:100)){
-    
-    if (row_ind == 1) {
-      if (col_ind == 1) {
-        if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
+    for (col_ind in c(1:100)) {
+      if (row_ind == 1) {
+        if (col_ind == 1) {
+          if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else if (col_ind == 100) {
+          if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else{
+          if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
         }
-      } else if (col_ind == 100) {
-        if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
+        
+      } else if (row_ind == 100) {
+        if (col_ind == 1) {
+          if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else if (col_ind == 100) {
+          if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else{
+          if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
         }
-      } else{
-        if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
+        
+      } else {
+        if (col_ind == 1) {
+          if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else if (col_ind == 100) {
+          if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
+        } else{
+          if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
+              df[row_ind, col_ind] < df[row_ind, col_ind - 1] &
+              df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
+            output_vec <- append(output_vec, df[row_ind, col_ind])
+            
+          }
         }
       }
-      
-    } else if (row_ind == 100) {
-      if (col_ind == 1) {
-        if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      } else if (col_ind == 100) {
-        if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      } else{
-        if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      }
-      
-    } else {
-      if (col_ind == 1) {
-        if (df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      } else if (col_ind == 100) {
-        if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      } else{
-        if (df[row_ind, col_ind] < df[row_ind - 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind + 1, col_ind] &
-            df[row_ind, col_ind] < df[row_ind, col_ind - 1] &
-            df[row_ind, col_ind] < df[row_ind, col_ind + 1]) {
-          output_vec <- append(output_vec, df[row_ind, col_ind])
-          
-        }
-      }
-    }
     }
     
   }
@@ -106,35 +98,35 @@ find_low_points <- function(df) {
 }
 
 
-result1<-sum(find_low_points(mat_num)+1)
+result1 <- sum(find_low_points(mat_num) + 1)
 
 #Part2
 
 
-nine_mat<-matrix(rep(9,102*102),ncol = 102,nrow = 102)
+nine_mat <- matrix(rep(9, 102 * 102), ncol = 102, nrow = 102)
 
 
-nine_mat[2:101,2:101]<-mat_num
+nine_mat[2:101, 2:101] <- mat_num
 
-seen_mat<-`dim<-`(nine_mat %in% c(9), dim(nine_mat))
+seen_mat <- `dim<-`(nine_mat %in% c(9), dim(nine_mat))
 
-basin_mat<-nine_mat
-basin_mat[nine_mat==9]<-0
-  
-basin_search<-function(df,row_ind,col_ind,basin_id){
-  seen_mat[row_ind,col_ind]<<-TRUE
-  basin_mat[row_ind,col_ind]<<-basin_id
-  if(!seen_mat[row_ind-1,col_ind]){
-    basin_mat<-basin_search(df,row_ind-1,col_ind,basin_id)
+basin_mat <- nine_mat
+basin_mat[nine_mat == 9] <- 0
+
+basin_search <- function(df, row_ind, col_ind, basin_id) {
+  seen_mat[row_ind, col_ind] <<- TRUE
+  basin_mat[row_ind, col_ind] <<- basin_id
+  if (!seen_mat[row_ind - 1, col_ind]) {
+    basin_mat <- basin_search(df, row_ind - 1, col_ind, basin_id)
   }
-  if(!seen_mat[row_ind+1,col_ind]){
-    basin_mat<-basin_search(df,row_ind+1,col_ind,basin_id)
+  if (!seen_mat[row_ind + 1, col_ind]) {
+    basin_mat <- basin_search(df, row_ind + 1, col_ind, basin_id)
   }
-  if(!seen_mat[row_ind,col_ind-1]){
-    basin_mat<-basin_search(df,row_ind,col_ind-1,basin_id)
+  if (!seen_mat[row_ind, col_ind - 1]) {
+    basin_mat <- basin_search(df, row_ind, col_ind - 1, basin_id)
   }
-  if(!seen_mat[row_ind,col_ind+1]){
-    basin_mat<-basin_search(df,row_ind,col_ind+1,basin_id)
+  if (!seen_mat[row_ind, col_ind + 1]) {
+    basin_mat <- basin_search(df, row_ind, col_ind + 1, basin_id)
   }
   basin_mat
 }
@@ -160,6 +152,4 @@ basin_lowpoint <- function(df) {
 }
 basin_lowpoint(nine_mat)
 
-
-
-
+result2 <-prod(sort(as.vector(table(basin_mat)), decreasing = TRUE)[2:4])
